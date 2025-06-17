9cfc55dac2df744a82951b61d620125b56441ea1
JoshFuji <156867710+JoshFuji@users.noreply.github.com>
2025-06-16 18:00:22 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import './App.css';
+import { useState } from "react";
+import './components/Flashcard';
+import FlashCard from './components/Flashcard';
+import flashcards from "./card.js";
+
+
+function App(){
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // Go to the next card
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === flashcards.length - 1 ? 0 : prevIndex + 1
+    );
+    console.log("bye")
+  };
+
+  // Go to the previous card
+  const handlePrevious = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? flashcards.length - 1 : prevIndex - 1
+    );
+  };
+
+  return (
+    <div className="App">
+      <h1>Common Japanese phrases</h1>
+      <p>Some common Japanese phrases that maybe helpful!</p>
+    
+    <div>
+      <h3>Number of cards: {flashcards.length}</h3>
+    </div>
+
+    <div className='card-container'>
+      <FlashCard
+        frontContent={<h2>{flashcards[currentIndex].frontContent}</h2>}
+        backContent={<h2>{flashcards[currentIndex].backContent}</h2>}
+      />
+    </div>
+
+      <button onClick={handlePrevious}>
+        Previous
+      </button>
+
+      <button onClick={handleNext}>
+        Next
+      </button>
+    </div>
+  )
+}
+
+export default App;
